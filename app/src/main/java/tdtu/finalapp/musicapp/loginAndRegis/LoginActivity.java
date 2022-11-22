799f90d4814997352d556a0df3f6989ce00d3ea6
Phtan1112 <tanphuocdt1@gmail.com>
2022-11-22 17:17:54 +0700

    add login test

---
+++ app/src/main/java/tdtu/finalapp/musicapp/loginAndRegis/LoginActivity.java
@@ -0,0 +1,16 @@
+package tdtu.finalapp.musicapp.loginAndRegis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import tdtu.finalapp.musicapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
